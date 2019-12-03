15295e0377de1fb8044ff5f4bfc585eaf19d228b
Gustavo Jordão <gustavojordao@Gustavos-MBP.lan>
2019-12-03 15:39:05 +0000

    Update dependency vulnerabilities

--- api/app.js
+++ api/app.js
@@ -28,7 +28,7 @@ app.use(function(req, res, next) {
 });
 // View engine setup
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+app.set("view engine", "pug");
 
 // Routes
 const indexRouter = require("./routes/index");
